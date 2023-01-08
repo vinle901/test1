2fd9ca151507e012542740e63da2c2f22dab7e24
vinle901 <94653461+vinle901@users.noreply.github.com>
2023-01-07 21:42:25 -0600

    Update first.py

--- first.py
+++ first.py
@@ -1,8 +1,4 @@
 print('hello world')
 print('cai deo gi vay')
 print('cai deo')
-<<<<<<< HEAD
-print('cai')
-=======
-print('con cat')
->>>>>>> cf7ca68860812cba06e89c9500a1536148644506
+print('cai con cat gi vay')
