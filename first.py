0119d769504184add2831442c3577f991a429eaf
vin <vinle901@gmail.com>
2023-01-11 00:01:34 -0600

    sua ti

--- first.py
+++ first.py
@@ -1,10 +1,15 @@
-print('hello world')
-print(2+3+6)
-print(2+4)
-print('tao met qua')
-print('vai qua')
-print('oi vai qua')
-print('new branch ')
-print('hihi')
-print('van dan')
-print('hoho')
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+x = Student("Mike", "Olsen")
+print(x.lastname)
